c8fde2b198740c2e9d8e3a6afdd6cdc3cf05afdc
lara <lara@ubuntu>
2017-05-16 07:18:27 -0700

    changing visualization errors

--- src/main/java/com/mycompany/frogsssa/service/serviceLayerService.java
+++ src/main/java/com/mycompany/frogsssa/service/serviceLayerService.java
@@ -51,7 +51,8 @@ public class serviceLayerService {
         //}
         //var = var.replace("/", ".");
         JsonNode obj = ((new ConnectionModule()).getValue(id, var));
-        if(obj==null || obj.toString().equals("null"))
+        System.out.println(obj);
+        if(obj==null || obj.asText().equals("null"))
             return Response.status(Response.Status.NOT_FOUND).build();
         return Response.ok(obj.toString()).build();
     }
